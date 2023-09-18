1930329f551e00148760811b271205824ac0ec77
Marcelo Hoffmann <hoffmannmarcelo@hotmail.com>
2023-09-18 13:48:21 -0300

    exemplo for

---
+++ exemplo-for/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+      for (int carneirinhos = 1; carneirinhos <=20; carneirinhos ++){
+        System.out.println(carneirinhos);
+      }
+      System.out.println("Joaozinho dormiu");
+    }
+}
